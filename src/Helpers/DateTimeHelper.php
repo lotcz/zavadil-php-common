0eecbcca5764bc54594d57b1aebad8138a81e105
Karel Zavadil <zavadil@incomaker.com>
2025-11-09 23:40:12 +0100

    more formats

--- src/Helpers/DateTimeHelper.php
+++ src/Helpers/DateTimeHelper.php
@@ -16,8 +16,19 @@ class DateTimeHelper {
 		'Y-m-d\TH:i:s.uP', /* ISO with microseconds and offset */
 		'Y-m-d\TH:i:sT', /* ISO with time zone */
 		'Y-m-d\TH:i:sP', /* ISO with offset */
-		DateTimeInterface::RFC7231 /* http dates */
+		DateTimeInterface::RFC7231, /* http dates */
+		DateTimeInterface::RSS,
 	];
+
+	public static function toTimezone(DateTimeInterface $date, string $timeZoneName): DateTimeInterface {
+		$gmtdate = DateTime::createFromInterface($date);
+		$gmtdate->setTimezone(new DateTimeZone($timeZoneName));
+		return $gmtdate;
+	}
+
+	public static function toGmt(DateTimeInterface $date): DateTimeInterface {
+		return self::toTimezone($date, 'GMT');
+	}
 
 	public static function parse(?string $str, bool $immutable = false): ?DateTimeInterface {
 		if (StringHelper::isBlank($str)) return null;
@@ -39,10 +50,20 @@ class DateTimeHelper {
 		return $date->format($format);
 	}
 
-	public static function formatForHttp(DateTimeInterface $date): ?string {
-		$gmtdate = DateTime::createFromInterface($date);
-		$gmtdate->setTimezone(new DateTimeZone('GMT'));
-		return $gmtdate->format(DateTimeInterface::RFC7231);
+	public static function formatForJson(DateTimeInterface $date): ?string {
+		return self::format($date, DateTimeInterface::RFC3339_EXTENDED);
+	}
+
+	public static function formatAsGmt(DateTimeInterface $date, string $format): string {
+		return self::format(self::toGmt($date), $format);
+	}
+
+	public static function formatForHttp(DateTimeInterface $date): string {
+		return self::formatAsGmt($date, DateTimeInterface::RFC7231);
+	}
+
+	public static function formatForRss(DateTimeInterface $date): string {
+		return self::formatAsGmt($date, DateTimeInterface::RSS);
 	}
 
 	public static function year(?DateTimeInterface $date = null): int {
