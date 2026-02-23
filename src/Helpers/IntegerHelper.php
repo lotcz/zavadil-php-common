de3c3bc0a4c9f22044caa1aa541a0eb2dab3c4a7
Karel Zavadil <zavadil@incomaker.com>
2026-02-23 20:29:23 +0100

    round

--- src/Helpers/IntegerHelper.php
+++ src/Helpers/IntegerHelper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Zavadil\Common\Helpers;
 
+use RoundingMode;
+
 class IntegerHelper {
 
 	public static function parse(?string $str, ?int $default = null): ?int {
@@ -13,6 +15,13 @@ class IntegerHelper {
 		$result = intval($trimmed);
 		if ($result === 0) return $default;
 		return $result;
+	}
+
+	public static function round(string|int|float|null $n, int $precision = 0, $mode = RoundingMode::HalfAwayFromZero): int|null {
+		if ($n === null) return null;
+		if (is_int($n)) return $n;
+		if (is_string($n)) return self::round(self::parse($n), $precision, $mode);
+		return (int)round($n, $precision, $mode);
 	}
 
 }
